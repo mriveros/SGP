55b3993ae00b41ff787eaabc962133cb05c402c6
mriveros <mriveros@mriveros>
2016-09-06 12:43:08 -0400

    -Arreglamos detalles de Autoria

--- web/logout.php
+++ web/logout.php
@@ -1,6 +1,6 @@
 <?php 
 	session_start(); 
 	session_destroy(); 
-        $ruta=$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']."/web";
+        $ruta=$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']."";
 	header("Location:http://$ruta/SGP/login/acceso.html");
 ?>
